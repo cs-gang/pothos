5b8ec3739cf3747198b7a6aceddd4836773d441e
anand2312 <anandkrishna2312@gmail.com>
2021-06-18 11:35:28 +0400

    feat: make models

--- src/app/models.py
+++ src/app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    """
+    Database model representing a Pothos user.
+    """
+
+    id = models.BigAutoField(primary_key=True)
+    username = models.CharField(max_length=30, null=False)
+    email = models.CharField(unique=True)
+
+
+class Transaction(models.Model):
+    """
+    Database model representing a Pothos user's transaction.
+    """
+
+    class TransactionType(models.TextChoices):
+        """
+        Enumeration representing the types of transactions possible.
+        """
+
+        INCOME = "income"
+        EXPENDITURE = "expenditure"
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    transaction_type = models.CharField(choices=TransactionType.choices)
+    amount = models.FloatField()
+    transaction_time = models.DateTimeField()
+    name = models.CharField()
+    notes = models.TextField(null=True)
